0de3f84166e71cadc2ebfec31b6f4cc3cfd457b1
sabari <sabarivasan323@gmail.com>
2025-04-07 09:57:06 +0530

    fix: fix mobile view

--- src/components/Experience/experience.jsx
+++ src/components/Experience/experience.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './experience.css';
 
 const technologies = [
@@ -11,17 +11,45 @@ const technologies = [
 ];
 
 const Experience = () => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <section className="experience-section" id='experience'>
       <h1>Experience</h1>
-      <p><strong>I've more than 1 years of experience in below technologies.</strong></p>
+      <p><strong>I've more than 1 year of experience in the below technologies.</strong></p>
       <div className="tech-icons">
-        {technologies.map((tech, index) => (
-          <div className="tech-item" key={index}>
-            <img src={tech.img} alt={tech.name} />
-            <p>{tech.name}</p>
-          </div>
-        ))}
+        {technologies.map((tech, index) => {
+          if (screenWidth > 1024) {
+            return (
+              <div key={index}>
+                <div className="tech-item">
+                  <img src={tech.img} alt={tech.name} />
+                  <p>{tech.name}</p>
+                </div>
+              </div>
+            );
+          } else if (screenWidth > 768) {
+            return<div key={index}>
+            <div className="tech-item">
+              <img src={tech.img} alt={tech.name} />
+              <p>{tech.name}</p>
+            </div>
+          </div>;
+          } else {
+            return (
+              <div className="tech-item" key={index}>
+                <img src={tech.img} alt={tech.name} />
+                <p>{tech.name}</p>
+              </div>
+            );
+          }
+        })}
       </div>
     </section>
   );
